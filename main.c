c8457aa195e502189decd1989c625a8807087461
Niroshan Rajadurai <niroshan@github.com>
2020-04-03 07:02:44 +0100

    Update main.c

--- main.c
+++ main.c
@@ -5,13 +5,19 @@ void congratulateUser(const char *userName)
         char buffer[80];
         /* BAD: this could overflow the buffer if the UserName is long */
         sprintf(buffer, "Congratulations, %s!", userName);
+        
+        printf("%s\n", buffer);
 }
 
 int main() {
+   char aName[256] = {0};
+        
    /* my first program in C */
-   printf("Hello, World! \n");
+   printf("What is your name?\n");
    
-   congratulateUser("A Very long name 123456789012345678911234567892123456789");
+   scanf("%s", aName);
+   
+   congratulateUser(aName);
    
    return 0;
 }
